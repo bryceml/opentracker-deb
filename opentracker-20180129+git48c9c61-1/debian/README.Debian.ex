3bd84c7653d05c4a623f2c521ea4c2f6d8909d14
Bryce Larson <brycelarsn@gmail.com>
2018-03-14 02:18:28 -0600

    hopefully done

--- opentracker-20180129+git48c9c61-1/debian/README.Debian.ex
+++ opentracker-20180129+git48c9c61-1/debian/README.Debian.ex
@@ -1,6 +1,6 @@
 opentracker for Debian
 ------------------------------------------
 
-For changes, please submit pull requests to https://github.com/bryceml/opentracker-deb
+<possible notes regarding this package - if none, delete this file>
 
  -- Bryce Larson <brycelarsn@gmail.com>  Tue, 13 Mar 2018 18:14:44 -0600
